c58138a739edda38638f3f72e8d0be52274623d4
Kokoster <dokokoster9@gmail.com>
2015-12-12 18:00:21 +0300

    changer resourse images for drums and guitar

--- src/model/band.cpp
+++ src/model/band.cpp
@@ -8,14 +8,14 @@
 Band::Band(QSize mainWindowSize)
 {
     Point guitarPosition(mainWindowSize.width()  / 8 - 50,
-                         mainWindowSize.height() / 2 + 50);
+                         mainWindowSize.height() / 2);
     std::unique_ptr<Instrument> guitar(new Instrument(":/instruments/instruments/Guitars/main_guitar/guitar.png",
                                                       ":/instruments/instruments/Guitars/main_guitar/disabled_guitar.png",
                                                       "qrc:/tracks/tracks/main_vocal_voice.mp3", guitarPosition));
 
     instruments.push_back(std::move(guitar));
 
-    Point drumsPosition(mainWindowSize.width() / 4 + 50,
+    Point drumsPosition(mainWindowSize.width() / 2,
                         mainWindowSize.height() / 2 - mainWindowSize.height() / 12);
     std::unique_ptr<Instrument> drums(new Instrument(":/instruments/instruments/Drums/drums.png",
                                                      ":/instruments/instruments/Drums/disabled_drums.png",
